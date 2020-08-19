715ebbda5fb46559296b82b1fc4ba56833695432
Tom Strempel <tom.strempel@mailbox.org>
2020-08-19 15:47:56 +0200

    Revert "cleanup"
    This reverts commit abeb6d213d9881dad58bbfaa46cee045ded2520b.

---
+++ src/main/java/org/jqassistant/contrib/plugin/kieker/impl/scanner/RecordConsumer.java
@@ -0,0 +1,54 @@
+package org.jqassistant.contrib.plugin.kieker.impl.scanner;
+
+import kieker.analysis.IProjectContext;
+import kieker.analysis.plugin.annotation.InputPort;
+import kieker.analysis.plugin.annotation.Plugin;
+import kieker.analysis.plugin.filter.AbstractFilterPlugin;
+import kieker.common.configuration.Configuration;
+import kieker.common.record.IMonitoringRecord;
+import kieker.common.record.flow.trace.TraceMetadata;
+import kieker.common.record.flow.trace.operation.AbstractOperationEvent;
+import kieker.common.record.misc.KiekerMetadataRecord;
+import kieker.common.record.system.*;
+
+// todo: use this instead of ListCollectionFilter
+@Plugin
+class RecordConsumer extends AbstractFilterPlugin {
+
+    public static final String INPUT_PORT_NAME = "newMonitoringRecord";
+    KiekerHelper kiekerHelper;
+
+    public RecordConsumer(Configuration configuration, IProjectContext projectContext, KiekerHelper kiekerHelper) {
+        super(configuration, projectContext);
+        this.kiekerHelper = kiekerHelper;
+    }
+
+    @InputPort(
+        name = RecordConsumer.INPUT_PORT_NAME,
+        eventTypes = { IMonitoringRecord.class }
+    )
+    public void newMonitoringRecord(final IMonitoringRecord iMonitoringRecord) {
+        if (iMonitoringRecord instanceof KiekerMetadataRecord) {
+            kiekerHelper.createRecord((KiekerMetadataRecord) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof TraceMetadata) {
+            kiekerHelper.createTrace((TraceMetadata) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof CPUUtilizationRecord) {
+            kiekerHelper.createCpuUtilizationRecord((CPUUtilizationRecord) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof DiskUsageRecord) {
+            kiekerHelper.createDiskUsageRecord((DiskUsageRecord) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof LoadAverageRecord) {
+            kiekerHelper.createLoadAverageRecord((LoadAverageRecord) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof MemSwapUsageRecord) {
+            kiekerHelper.createMemSwapUsageRecord((MemSwapUsageRecord) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof NetworkUtilizationRecord) {
+            kiekerHelper.createNetworkUtilizationRecord((NetworkUtilizationRecord) iMonitoringRecord);
+        } else if (iMonitoringRecord instanceof AbstractOperationEvent) {
+            kiekerHelper.createEvent((AbstractOperationEvent) iMonitoringRecord);
+        }
+    }
+
+    @Override
+    public Configuration getCurrentConfiguration() {
+        return new Configuration();
+    }
+}
